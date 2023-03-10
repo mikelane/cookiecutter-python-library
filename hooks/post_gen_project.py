47763051764227b19766d4e2480ff6a82ba9af26
Mike Lane <mikelane@gmail.com>
2023-03-10 08:43:00 -0800

    Update hooks, add optional logging to hooks, and clean up dependencies

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,20 @@
 """Post generation hook for cookiecutter-pypackage-minimal."""
 from __future__ import annotations
 
+import logging
+import os
 import shlex
 import subprocess
+
+is_debug = os.getenv('DEBUG', '0') == '1'
+
+logging.basicConfig(
+    level=logging.DEBUG if is_debug else logging.INFO,
+    format='%(asctime)s %(name)-12s | %(levelname)-8s | %(message)s',
+    datefmt='%H:%M:%S',
+)
+
+logger = logging.getLogger('Post Gen Project Hook')
 
 
 # TODO: If cookiecutter allows you to import from the local hooks directory,
@@ -12,13 +24,14 @@ def stream_shell_output(command: str) -> None:
     """Stream the output of a shell command to stdout."""
     with subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
         for line in iter(proc.stdout.readline, b''):
-            print(line.decode('utf-8').rstrip())
+            logger.debug(line.decode('utf-8').rstrip())
 
 
-print(f'{" Post Gen Project Hook ":=^80}')
+logger.debug('Initializing git repo')
+stream_shell_output('git init')
 
-print(f'{" Installing dependencies ":-^50}')
+logger.debug(' Installing dependencies')
 stream_shell_output('poetry install')
 
-print(f'{" Installing pre-commit hooks ":-^50}')
+logger.debug('Installing pre-commit hooks')
 stream_shell_output('poetry run pre-commit install')
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,20 +1,29 @@
 """Pre-Generate Project Hook."""
 from __future__ import annotations
 
-import shlex
-import subprocess
+import logging
+import os
+import re
+import sys
+
+is_debug = os.getenv('DEBUG', '0') == '1'
+
+logging.basicConfig(
+    level=logging.DEBUG if is_debug else logging.INFO,
+    format='%(asctime)s %(name)-12s | %(levelname)-8s | %(message)s',
+    datefmt='%H:%M:%S',
+)
+
+logger = logging.getLogger('Pre Gen Project Hook')
 
 
-# TODO: If cookiecutter allows you to import from the local hooks directory,
-#  then move this function to a shared module.
-# Related: https://github.com/cookiecutter/cookiecutter/issues/824
-def stream_shell_output(command: str) -> None:
-    """Stream the output of a shell command to stdout."""
-    with subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
-        for line in iter(proc.stdout.readline, b''):
-            print(line.decode('utf-8').rstrip())
+def _has_valid_project_name(project_name: str) -> bool:
+    regex = re.compile(r'^[a-zA-Z][-\w\s]*$')
+    return bool(regex.match(project_name))
 
 
-print(f'{" Post Gen Project Hook ":=^80}')
-print(f'{" Initializing git repo ":-^50}')
-stream_shell_output('git init')
+if not _has_valid_project_name('{{ cookiecutter.project_name }}'):
+    logger.error('Invalid project name')
+    sys.exit(1)
+else:
+    logger.debug('Project name is valid')
