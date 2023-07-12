1d6b2e0996584b2f1040b32dab7b07f48ca82335
Mike Lane <mikelane@gmail.com>
2023-07-11 17:40:38 -0700

    Fix type hints in test_main.py
    Mypy fails because the test doesn't have a return type.

--- {{ cookiecutter.__project_name }}/tests/test_{{ cookiecutter.__package_name }}/test_main.py
+++ {{ cookiecutter.__project_name }}/tests/test_{{ cookiecutter.__package_name }}/test_main.py
@@ -1,6 +1,6 @@
 from {{ cookiecutter.__package_name }}.main import get_hello
 
-def it_prints_hi_to_the_project_author():
+def it_prints_hi_to_the_project_author() -> None:
     expected = 'Hello, {{ cookiecutter.author_name }}!'
     actual = get_hello('{{ cookiecutter.author_name }}')
     assert actual == expected
